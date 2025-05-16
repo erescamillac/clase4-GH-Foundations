164e7febcddecbaede65aea9d80e8936b3cc02a3
Erick Escamilla Charco <erescamillac@gmail.com>
2025-05-15 21:11:21 -0600

    Se agrega el archivo [Customers.cs] encargado del Módulo de [Clientes], originalmente sobre la local-branch [feature-costumers]

---
+++ Customers.cs
@@ -0,0 +1,9 @@
+// Esta clase es la encargada principal del Módulo de [Clientes]
+// *inicialmente solo sobre la rama-local (local-branch) 'feature-costumers'
+
+using System;
+
+Console.WriteLine("Menú de Clientes");
+Console.WriteLine("1. Obtener lista de clientes");
+Console.WriteLine("2. Agregar nuevo cliente");
+Console.WriteLine("3. Salir");
